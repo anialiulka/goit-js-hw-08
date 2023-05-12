ab869537d720087dd72949c3bde363635a0c0162
ania.liulka <anialiulka@gmail.com>
2023-05-12 13:02:27 -0400

    Finished task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const time = function (data) {
+  let timeWatched = data;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(timeWatched));
+};
+
+const throttledTime = throttle(time, 1000);
+
+player.on('timeupdate', throttledTime);
+
+const newStartTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player.setCurrentTime(newStartTime.seconds);
